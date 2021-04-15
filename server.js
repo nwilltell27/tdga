c5ea80407615f02f052f130af40fb477d6aab347
Nathan Tellers <nathantellers@gmail.com>
2021-04-14 21:24:48 -0700

    home page created

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const port = 3000;
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+app.listen(port, function() {
+    console.log(`Express is listening on port:${port}`);
+});
