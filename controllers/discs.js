6744d1084a7ddefd2844648bd116c4a97e374130
Nathan Tellers <nathantellers@gmail.com>
2021-04-20 19:49:59 -0700

    wip: adds data to mongoDB via new disc form

--- controllers/discs.js
+++ controllers/discs.js
@@ -1,9 +1,20 @@
-
+const Disc = require('../models/disc');
 
 module.exports = {
     new: newDisc,
+    create,
 };
 
 function newDisc(req, res) {
     res.render('discs/new');
+}
+
+function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key];
+  }
+  Disc.create(req.body, function (err, disc) {
+    if (err) return res.redirect('/discs/new');
+    res.redirect('/discs');
+  });
 }
--- routes/discs.js
+++ routes/discs.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const discsCtrl = require('../controllers/discs');
 
 router.get('/new', discsCtrl.new);
+router.post('/', discsCtrl.create);
 
 module.exports = router;
